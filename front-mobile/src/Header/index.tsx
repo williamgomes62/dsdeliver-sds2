3643f59976a5e4335e73624ded07bccba8da99c6
William Gomes <williamlimagomes62@gmail.com>
2021-01-12 13:23:23 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,13 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { requireNativeComponent, StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 function Header() {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+    navigation.navigate('Home');
+  }
+
   return (
-    <View style={styles.container}>
-        <Image source={require('../assets/logo.png')} />
-        <Text style={styles.text}>DS Delivery</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+        <View style={styles.container}>
+          <Image source={require('../assets/logo.png')} />
+          <Text style={styles.text}>DS Delivery</Text>
+        </View>
+    </TouchableWithoutFeedback>
+    
   );
 }
 
